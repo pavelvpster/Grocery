6efec63772ce4ab43f8d0276f8d1deae37f6d87f
Pavel Prokhorov <pavelvpster@gmail.com>
2016-09-13 23:05:43 +0600

    Implement Item list UI

--- src/main/java/org/interactiverobotics/grocery/web/ItemWebController.java
+++ src/main/java/org/interactiverobotics/grocery/web/ItemWebController.java
@@ -20,8 +20,17 @@
 
 package org.interactiverobotics.grocery.web;
 
+import org.interactiverobotics.grocery.domain.Item;
+import org.interactiverobotics.grocery.repository.ItemRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Item web controller.
@@ -30,9 +39,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/item")
 public class ItemWebController {
 
+    private final ItemRepository itemRepository;
+
+    public ItemWebController(final ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
+
     @RequestMapping("/")
     public String index() {
         return "item";
+    }
+
+    @RequestMapping("/list")
+    public String getItemList(@RequestParam(value = "page", defaultValue = "1") Integer pageNumber,
+                              @RequestParam(value = "size", defaultValue = "10") Integer pageSize, Model model) {
+
+        final PageRequest pageRequest = new PageRequest(pageNumber - 1, pageSize);
+        final Page<Item> page = this.itemRepository.findAll(pageRequest);
+
+        final List<Item> items = new ArrayList<>();
+        page.forEach(item -> items.add(item));
+
+        model.addAttribute("offset", 1 + (pageNumber - 1) * pageSize);
+        model.addAttribute("items", items);
+        model.addAttribute("page_count", page.getTotalPages());
+        model.addAttribute("current_page", pageNumber);
+
+        return "item_list";
     }
 
 }
--- src/main/resources/public/item.js
+++ src/main/resources/public/item.js
@@ -1,4 +1,15 @@
 $(function() {
+
     $(".nav").find(".active").removeClass("active");
     $(".nav").find("#item").parent().addClass("active");
+
+    $.get("/item/list", {}, function(result) {
+        $("#item_list").html(result);
+    });
 });
+
+function gotoPage(page) {
+    $.get("/item/list?page=" + page, {}, function(result) {
+        $("#item_list").html(result);
+    });
+}
