0c3582111c80f7865b671700ca509d206eb2df5e
Pavel Prokhorov <pavelvpster@gmail.com>
2016-10-04 20:23:19 +0600

    Implement ShoppingList UI

---
+++ src/main/resources/public/shopping_list.js
@@ -0,0 +1,39 @@
+$(function() {
+
+    $(".nav").find(".active").removeClass("active");
+    $(".nav").find("#shopping-list").parent().addClass("active");
+
+    initializePagination("/shopping_list/list", "#shopping-list-list-container");
+});
+
+function showCreateShoppingListForm() {
+    $.get("/shopping_list/form", {}, function(result) {
+        $("#shopping-list-form-container").html(result);
+        $("#shopping-list-form").modal();
+    });
+}
+
+function showUpdateShoppingListForm(id) {
+    $.get("/shopping_list/" + id + "/form", {}, function(result) {
+        $("#shopping-list-form-container").html(result);
+        $("#shopping-list-form").modal();
+    });
+}
+
+function validateShoppingListForm() {
+    var name = $("#shopping-list-properties #name").val();
+    if (name == null || name.length < 1 || name.length > 255) {
+        $("#name-error").text("Name must be 1 to 255 characters.");
+        return false;
+    }
+}
+
+function deleteShoppingList(id) {
+    $.ajax({
+        type: 'DELETE',
+        url: "/shopping_list/" + id,
+        success: function(result) {
+            window.location = "/shopping_list/";
+        }
+    });
+}
