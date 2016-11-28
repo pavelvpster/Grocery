c09f524482719cf558b2d693870b783e68c1bb1a
Pavel Prokhorov <pavelvpster@gmail.com>
2016-11-28 20:53:01 +0600

    Update database script

--- sql/grocery.sql
+++ sql/grocery.sql
@@ -2,9 +2,10 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 9.3.14
--- Dumped by pg_dump version 9.3.14
--- Started on 2016-08-31 12:12:34 OMST
+-- Dumped from database version 9.5.4
+-- Dumped by pg_dump version 9.5.4
+
+-- Started on 2016-09-30 22:06:16 OMST
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -12,9 +13,10 @@ SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SET check_function_bodies = false;
 SET client_min_messages = warning;
+SET row_security = off;
 
 --
--- TOC entry 8 (class 2615 OID 47502)
+-- TOC entry 7 (class 2615 OID 16439)
 -- Name: grocery; Type: SCHEMA; Schema: -; Owner: grocery
 --
 
@@ -24,7 +26,7 @@ CREATE SCHEMA grocery;
 ALTER SCHEMA grocery OWNER TO grocery;
 
 --
--- TOC entry 1 (class 3079 OID 11787)
+-- TOC entry 1 (class 3079 OID 12397)
 -- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
 --
 
@@ -32,7 +34,7 @@ CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 
 
 --
--- TOC entry 2018 (class 0 OID 0)
+-- TOC entry 2214 (class 0 OID 0)
 -- Dependencies: 1
 -- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
 --
@@ -43,7 +45,7 @@ COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
 SET search_path = grocery, pg_catalog;
 
 --
--- TOC entry 173 (class 1259 OID 47506)
+-- TOC entry 182 (class 1259 OID 16440)
 -- Name: item_id_seq; Type: SEQUENCE; Schema: grocery; Owner: grocery
 --
 
@@ -55,15 +57,15 @@ CREATE SEQUENCE item_id_seq
     CACHE 1;
 
 
-ALTER TABLE grocery.item_id_seq OWNER TO grocery;
+ALTER TABLE item_id_seq OWNER TO grocery;
 
 SET default_tablespace = '';
 
 SET default_with_oids = false;
 
 --
--- TOC entry 172 (class 1259 OID 47503)
--- Name: items; Type: TABLE; Schema: grocery; Owner: grocery; Tablespace: 
+-- TOC entry 183 (class 1259 OID 16442)
+-- Name: items; Type: TABLE; Schema: grocery; Owner: grocery
 --
 
 CREATE TABLE items (
@@ -72,10 +74,10 @@ CREATE TABLE items (
 );
 
 
-ALTER TABLE grocery.items OWNER TO grocery;
+ALTER TABLE items OWNER TO grocery;
 
 --
--- TOC entry 179 (class 1259 OID 47602)
+-- TOC entry 184 (class 1259 OID 16446)
 -- Name: purchase_id_seq; Type: SEQUENCE; Schema: grocery; Owner: grocery
 --
 
@@ -87,11 +89,11 @@ CREATE SEQUENCE purchase_id_seq
     CACHE 1;
 
 
-ALTER TABLE grocery.purchase_id_seq OWNER TO grocery;
+ALTER TABLE purchase_id_seq OWNER TO grocery;
 
 --
--- TOC entry 178 (class 1259 OID 47599)
--- Name: purchases; Type: TABLE; Schema: grocery; Owner: grocery; Tablespace: 
+-- TOC entry 185 (class 1259 OID 16448)
+-- Name: purchases; Type: TABLE; Schema: grocery; Owner: grocery
 --
 
 CREATE TABLE purchases (
@@ -103,10 +105,10 @@ CREATE TABLE purchases (
 );
 
 
-ALTER TABLE grocery.purchases OWNER TO grocery;
+ALTER TABLE purchases OWNER TO grocery;
 
 --
--- TOC entry 175 (class 1259 OID 47512)
+-- TOC entry 186 (class 1259 OID 16452)
 -- Name: shop_id_seq; Type: SEQUENCE; Schema: grocery; Owner: grocery
 --
 
@@ -118,11 +120,69 @@ CREATE SEQUENCE shop_id_seq
     CACHE 1;
 
 
-ALTER TABLE grocery.shop_id_seq OWNER TO grocery;
+ALTER TABLE shop_id_seq OWNER TO grocery;
 
 --
--- TOC entry 174 (class 1259 OID 47509)
--- Name: shops; Type: TABLE; Schema: grocery; Owner: grocery; Tablespace: 
+-- TOC entry 190 (class 1259 OID 16490)
+-- Name: shopping_list_id_seq; Type: SEQUENCE; Schema: grocery; Owner: grocery
+--
+
+CREATE SEQUENCE shopping_list_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE shopping_list_id_seq OWNER TO grocery;
+
+--
+-- TOC entry 192 (class 1259 OID 16498)
+-- Name: shopping_list_item_id_seq; Type: SEQUENCE; Schema: grocery; Owner: grocery
+--
+
+CREATE SEQUENCE shopping_list_item_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE shopping_list_item_id_seq OWNER TO grocery;
+
+--
+-- TOC entry 193 (class 1259 OID 16500)
+-- Name: shopping_list_items; Type: TABLE; Schema: grocery; Owner: grocery
+--
+
+CREATE TABLE shopping_list_items (
+    id bigint DEFAULT nextval('shopping_list_item_id_seq'::regclass) NOT NULL,
+    shopping_list_id bigint NOT NULL,
+    item_id bigint NOT NULL,
+    quantity bigint DEFAULT 1
+);
+
+
+ALTER TABLE shopping_list_items OWNER TO grocery;
+
+--
+-- TOC entry 191 (class 1259 OID 16492)
+-- Name: shopping_lists; Type: TABLE; Schema: grocery; Owner: grocery
+--
+
+CREATE TABLE shopping_lists (
+    id bigint DEFAULT nextval('shopping_list_id_seq'::regclass) NOT NULL,
+    name character varying(255) NOT NULL
+);
+
+
+ALTER TABLE shopping_lists OWNER TO grocery;
+
+--
+-- TOC entry 187 (class 1259 OID 16454)
+-- Name: shops; Type: TABLE; Schema: grocery; Owner: grocery
 --
 
 CREATE TABLE shops (
@@ -131,10 +191,10 @@ CREATE TABLE shops (
 );
 
 
-ALTER TABLE grocery.shops OWNER TO grocery;
+ALTER TABLE shops OWNER TO grocery;
 
 --
--- TOC entry 177 (class 1259 OID 47587)
+-- TOC entry 188 (class 1259 OID 16458)
 -- Name: visit_id_seq; Type: SEQUENCE; Schema: grocery; Owner: grocery
 --
 
@@ -146,26 +206,27 @@ CREATE SEQUENCE visit_id_seq
     CACHE 1;
 
 
-ALTER TABLE grocery.visit_id_seq OWNER TO grocery;
+ALTER TABLE visit_id_seq OWNER TO grocery;
 
 --
--- TOC entry 176 (class 1259 OID 47584)
--- Name: visits; Type: TABLE; Schema: grocery; Owner: grocery; Tablespace: 
+-- TOC entry 189 (class 1259 OID 16460)
+-- Name: visits; Type: TABLE; Schema: grocery; Owner: grocery
 --
 
 CREATE TABLE visits (
     id bigint DEFAULT nextval('visit_id_seq'::regclass) NOT NULL,
     shop_id bigint NOT NULL,
     started timestamp without time zone,
-    completed timestamp without time zone
+    completed timestamp without time zone,
+    shopping_list_id bigint
 );
 
 
-ALTER TABLE grocery.visits OWNER TO grocery;
+ALTER TABLE visits OWNER TO grocery;
 
 --
--- TOC entry 2019 (class 0 OID 0)
--- Dependencies: 173
+-- TOC entry 2215 (class 0 OID 0)
+-- Dependencies: 182
 -- Name: item_id_seq; Type: SEQUENCE SET; Schema: grocery; Owner: grocery
 --
 
@@ -173,8 +234,8 @@ SELECT pg_catalog.setval('item_id_seq', 1, false);
 
 
 --
--- TOC entry 2003 (class 0 OID 47503)
--- Dependencies: 172
+-- TOC entry 2196 (class 0 OID 16442)
+-- Dependencies: 183
 -- Data for Name: items; Type: TABLE DATA; Schema: grocery; Owner: grocery
 --
 
@@ -183,8 +244,8 @@ COPY items (id, name) FROM stdin;
 
 
 --
--- TOC entry 2020 (class 0 OID 0)
--- Dependencies: 179
+-- TOC entry 2216 (class 0 OID 0)
+-- Dependencies: 184
 -- Name: purchase_id_seq; Type: SEQUENCE SET; Schema: grocery; Owner: grocery
 --
 
@@ -192,8 +253,8 @@ SELECT pg_catalog.setval('purchase_id_seq', 1, false);
 
 
 --
--- TOC entry 2009 (class 0 OID 47599)
--- Dependencies: 178
+-- TOC entry 2198 (class 0 OID 16448)
+-- Dependencies: 185
 -- Data for Name: purchases; Type: TABLE DATA; Schema: grocery; Owner: grocery
 --
 
@@ -202,8 +263,8 @@ COPY purchases (id, visit_id, item_id, quantity, price) FROM stdin;
 
 
 --
--- TOC entry 2021 (class 0 OID 0)
--- Dependencies: 175
+-- TOC entry 2217 (class 0 OID 0)
+-- Dependencies: 186
 -- Name: shop_id_seq; Type: SEQUENCE SET; Schema: grocery; Owner: grocery
 --
 
@@ -211,8 +272,46 @@ SELECT pg_catalog.setval('shop_id_seq', 1, false);
 
 
 --
--- TOC entry 2005 (class 0 OID 47509)
--- Dependencies: 174
+-- TOC entry 2218 (class 0 OID 0)
+-- Dependencies: 190
+-- Name: shopping_list_id_seq; Type: SEQUENCE SET; Schema: grocery; Owner: grocery
+--
+
+SELECT pg_catalog.setval('shopping_list_id_seq', 1, false);
+
+
+--
+-- TOC entry 2219 (class 0 OID 0)
+-- Dependencies: 192
+-- Name: shopping_list_item_id_seq; Type: SEQUENCE SET; Schema: grocery; Owner: grocery
+--
+
+SELECT pg_catalog.setval('shopping_list_item_id_seq', 1, false);
+
+
+--
+-- TOC entry 2206 (class 0 OID 16500)
+-- Dependencies: 193
+-- Data for Name: shopping_list_items; Type: TABLE DATA; Schema: grocery; Owner: grocery
+--
+
+COPY shopping_list_items (id, shopping_list_id, item_id, quantity) FROM stdin;
+\.
+
+
+--
+-- TOC entry 2204 (class 0 OID 16492)
+-- Dependencies: 191
+-- Data for Name: shopping_lists; Type: TABLE DATA; Schema: grocery; Owner: grocery
+--
+
+COPY shopping_lists (id, name) FROM stdin;
+\.
+
+
+--
+-- TOC entry 2200 (class 0 OID 16454)
+-- Dependencies: 187
 -- Data for Name: shops; Type: TABLE DATA; Schema: grocery; Owner: grocery
 --
 
@@ -221,8 +320,8 @@ COPY shops (id, name) FROM stdin;
 
 
 --
--- TOC entry 2022 (class 0 OID 0)
--- Dependencies: 177
+-- TOC entry 2220 (class 0 OID 0)
+-- Dependencies: 188
 -- Name: visit_id_seq; Type: SEQUENCE SET; Schema: grocery; Owner: grocery
 --
 
@@ -230,18 +329,18 @@ SELECT pg_catalog.setval('visit_id_seq', 1, false);
 
 
 --
--- TOC entry 2007 (class 0 OID 47584)
--- Dependencies: 176
+-- TOC entry 2202 (class 0 OID 16460)
+-- Dependencies: 189
 -- Data for Name: visits; Type: TABLE DATA; Schema: grocery; Owner: grocery
 --
 
-COPY visits (id, shop_id, started, completed) FROM stdin;
+COPY visits (id, shop_id, started, completed, shopping_list_id) FROM stdin;
 \.
 
 
 --
--- TOC entry 1883 (class 2606 OID 47521)
--- Name: item_pk; Type: CONSTRAINT; Schema: grocery; Owner: grocery; Tablespace: 
+-- TOC entry 2060 (class 2606 OID 16465)
+-- Name: item_pk; Type: CONSTRAINT; Schema: grocery; Owner: grocery
 --
 
 ALTER TABLE ONLY items
@@ -249,8 +348,8 @@ ALTER TABLE ONLY items
 
 
 --
--- TOC entry 1892 (class 2606 OID 47605)
--- Name: purchase_pk; Type: CONSTRAINT; Schema: grocery; Owner: grocery; Tablespace: 
+-- TOC entry 2064 (class 2606 OID 16467)
+-- Name: purchase_pk; Type: CONSTRAINT; Schema: grocery; Owner: grocery
 --
 
 ALTER TABLE ONLY purchases
@@ -258,8 +357,8 @@ ALTER TABLE ONLY purchases
 
 
 --
--- TOC entry 1885 (class 2606 OID 47523)
--- Name: shop_pk; Type: CONSTRAINT; Schema: grocery; Owner: grocery; Tablespace: 
+-- TOC entry 2066 (class 2606 OID 16469)
+-- Name: shop_pk; Type: CONSTRAINT; Schema: grocery; Owner: grocery
 --
 
 ALTER TABLE ONLY shops
@@ -267,8 +366,26 @@ ALTER TABLE ONLY shops
 
 
 --
--- TOC entry 1888 (class 2606 OID 47590)
--- Name: visit_pk; Type: CONSTRAINT; Schema: grocery; Owner: grocery; Tablespace: 
+-- TOC entry 2075 (class 2606 OID 16504)
+-- Name: shopping_list_item_pk; Type: CONSTRAINT; Schema: grocery; Owner: grocery
+--
+
+ALTER TABLE ONLY shopping_list_items
+    ADD CONSTRAINT shopping_list_item_pk PRIMARY KEY (id);
+
+
+--
+-- TOC entry 2071 (class 2606 OID 16496)
+-- Name: shopping_list_pk; Type: CONSTRAINT; Schema: grocery; Owner: grocery
+--
+
+ALTER TABLE ONLY shopping_lists
+    ADD CONSTRAINT shopping_list_pk PRIMARY KEY (id);
+
+
+--
+-- TOC entry 2069 (class 2606 OID 16471)
+-- Name: visit_pk; Type: CONSTRAINT; Schema: grocery; Owner: grocery
 --
 
 ALTER TABLE ONLY visits
@@ -276,31 +393,47 @@ ALTER TABLE ONLY visits
 
 
 --
--- TOC entry 1889 (class 1259 OID 47630)
--- Name: fki_purchase_item_fk; Type: INDEX; Schema: grocery; Owner: grocery; Tablespace: 
+-- TOC entry 2061 (class 1259 OID 16472)
+-- Name: fki_purchase_item_fk; Type: INDEX; Schema: grocery; Owner: grocery
 --
 
 CREATE INDEX fki_purchase_item_fk ON purchases USING btree (item_id);
 
 
 --
--- TOC entry 1890 (class 1259 OID 47624)
--- Name: fki_purchase_visit_fk; Type: INDEX; Schema: grocery; Owner: grocery; Tablespace: 
+-- TOC entry 2062 (class 1259 OID 16473)
+-- Name: fki_purchase_visit_fk; Type: INDEX; Schema: grocery; Owner: grocery
 --
 
 CREATE INDEX fki_purchase_visit_fk ON purchases USING btree (visit_id);
 
 
 --
--- TOC entry 1886 (class 1259 OID 47598)
--- Name: fki_visit_shop_fk; Type: INDEX; Schema: grocery; Owner: grocery; Tablespace: 
+-- TOC entry 2072 (class 1259 OID 16518)
+-- Name: fki_shopping_list_item_item_fk; Type: INDEX; Schema: grocery; Owner: grocery
+--
+
+CREATE INDEX fki_shopping_list_item_item_fk ON shopping_list_items USING btree (item_id);
+
+
+--
+-- TOC entry 2073 (class 1259 OID 16512)
+-- Name: fki_shopping_list_item_shopping_list_fk; Type: INDEX; Schema: grocery; Owner: grocery
+--
+
+CREATE INDEX fki_shopping_list_item_shopping_list_fk ON shopping_list_items USING btree (shopping_list_id);
+
+
+--
+-- TOC entry 2067 (class 1259 OID 16474)
+-- Name: fki_visit_shop_fk; Type: INDEX; Schema: grocery; Owner: grocery
 --
 
 CREATE INDEX fki_visit_shop_fk ON visits USING btree (shop_id);
 
 
 --
--- TOC entry 1895 (class 2606 OID 47625)
+-- TOC entry 2076 (class 2606 OID 16475)
 -- Name: purchase_item_fk; Type: FK CONSTRAINT; Schema: grocery; Owner: grocery
 --
 
@@ -309,7 +442,7 @@ ALTER TABLE ONLY purchases
 
 
 --
--- TOC entry 1894 (class 2606 OID 47619)
+-- TOC entry 2077 (class 2606 OID 16480)
 -- Name: purchase_visit_fk; Type: FK CONSTRAINT; Schema: grocery; Owner: grocery
 --
 
@@ -318,7 +451,25 @@ ALTER TABLE ONLY purchases
 
 
 --
--- TOC entry 1893 (class 2606 OID 47593)
+-- TOC entry 2080 (class 2606 OID 16513)
+-- Name: shopping_list_item_item_fk; Type: FK CONSTRAINT; Schema: grocery; Owner: grocery
+--
+
+ALTER TABLE ONLY shopping_list_items
+    ADD CONSTRAINT shopping_list_item_item_fk FOREIGN KEY (item_id) REFERENCES items(id) ON DELETE CASCADE;
+
+
+--
+-- TOC entry 2079 (class 2606 OID 16507)
+-- Name: shopping_list_item_shopping_list_fk; Type: FK CONSTRAINT; Schema: grocery; Owner: grocery
+--
+
+ALTER TABLE ONLY shopping_list_items
+    ADD CONSTRAINT shopping_list_item_shopping_list_fk FOREIGN KEY (shopping_list_id) REFERENCES shopping_lists(id) ON DELETE CASCADE;
+
+
+--
+-- TOC entry 2078 (class 2606 OID 16485)
 -- Name: visit_shop_fk; Type: FK CONSTRAINT; Schema: grocery; Owner: grocery
 --
 
@@ -327,8 +478,8 @@ ALTER TABLE ONLY visits
 
 
 --
--- TOC entry 2017 (class 0 OID 0)
--- Dependencies: 6
+-- TOC entry 2213 (class 0 OID 0)
+-- Dependencies: 8
 -- Name: public; Type: ACL; Schema: -; Owner: postgres
 --
 
@@ -338,7 +489,7 @@ GRANT ALL ON SCHEMA public TO postgres;
 GRANT ALL ON SCHEMA public TO PUBLIC;
 
 
--- Completed on 2016-08-31 12:12:34 OMST
+-- Completed on 2016-09-30 22:06:17 OMST
 
 --
 -- PostgreSQL database dump complete
